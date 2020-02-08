69039367dbe12355226e876811d7fd940ba13176
Richard-Walter <willthisnamedo@gmail.com>
2020-02-09 10:24:49 +1100

    added line number to updated fixed file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """
-This program is assists in survey CompNet adjustments.
+
+This program assists in survey CompNet adjustments.
 
 Written by Richard Walter 2020
+
 """
 
 import re
@@ -12,68 +14,62 @@ class FixedFile:
     def __init__(self, fixed_file_path):
 
         self.fixed_file_path = fixed_file_path
-        self.file_contents = None
-        self.updated_file_contents = ""
+        self.fixed_file_contents = None
         self.station_list = []
+        self.updated_file_contents = ""
 
         with open(fixed_file_path, 'r') as f_orig:
-            self.file_contents = f_orig.readlines()
+            self.fixed_file_contents = f_orig.readlines()
 
-    # returns a list of stations from the fixed file that need need to be updated
-    def get_station_list(self):
+    @staticmethod
+    def get_station(line):
+
+        station = "UNKNOWN"
 
         # Line number is at the start of a string and contains digits followed by whiespace
         re_pattern = re.compile(r'"\w+"')
+        match = re_pattern.search(line)
 
-        for line in self.file_contents:
-            match = re_pattern.search(line)
+        # strip of quotation marks and add to station list
+        if match is not None:
+            station = match.group()[1:-1]
 
-            # strip of quotation marks and add to station list
-            self.station_list.append(match.group()[1:-1])
+        return station
 
-        return self.station_list
+    @staticmethod
+    def get_line_number(line):
 
-    def get_line_number(self, station):
+        line_number = "???"
 
         # Line number is at the start of a line
         re_pattern = re.compile(r'^\d+\s')
-        line_number = "???"
 
-        for line in self.file_contents:
+        match = re_pattern.search(line)
 
-            if station in line:
-                match = re_pattern.search(line)
-                line_number = match.group()
-                break
+        if match:
+            line_number = match.group().strip()
 
         return line_number
 
-    def update(self, coordinate_contents):
+    def update(self, coordinate_file):
 
-        re_pattern_easting = re.compile(r'\b28\d{4}\.\d{4}')
-        re_pattern_northing = re.compile(r'\b62\d{5}\.\d{4}')
-        updated_file_dictionary = {}
+        for line in self.fixed_file_contents:
 
-        self.station_list = self.get_station_list()
+            # Get coordinates for this station if exists in the coordinate file
+            station = self.get_station(line)
 
-        # check each line of the coordinate contents to see if it contains a matching station.
-        for coordinate_contents_line in coordinate_contents:
-            for station in self.station_list:
-                if station in coordinate_contents_line:
-                    # grab easting and northing for this station
-                    easting_match = re_pattern_easting.search(coordinate_contents_line)
-                    northing_match = re_pattern_northing.search(coordinate_contents_line)
+            coordinate_dict = coordinate_file.get_coordinates(station)
 
-                    # create a dictionary so that we can later order by line number and write out to file
-                    updated_file_dictionary[int(self.get_line_number(station))] = easting_match.group() + '  ' + \
-                                                                                  northing_match.group() + ' ' + '"'\
-                                                                                  + station +'"\n'
-                    break
+            # update fixed_file coordinate if a match was found
+            if coordinate_dict:
+                easting = coordinate_dict['Eastings']
+                northing = coordinate_dict['Northings']
 
-        # order updated file contents by line number
-        for line_number in sorted(updated_file_dictionary):
-            update_file_line = str(line_number) + ' ' + updated_file_dictionary[line_number]
-            self.updated_file_contents += update_file_line
+                updated_line = self.get_line_number(line) + ' ' + easting + '  ' + northing + ' "' + station + '"\n'
+                self.updated_file_contents += updated_line
+
+            else:
+                self.updated_file_contents += line
 
         print(self.updated_file_contents)
 
@@ -99,13 +95,34 @@ class CoordinateFile:
         else:
             pass
 
+    def get_coordinates(self, station):
+
+        re_pattern_easting = re.compile(r'\b28\d{4}\.\d{4}')
+        re_pattern_northing = re.compile(r'\b62\d{5}\.\d{4}')
+
+        coordinate_dict = {}
+
+        # check each line of the coordinate contents to see if it contains a matching station.
+        for coordinate_contents_line in self.file_contents:
+            if station in coordinate_contents_line:
+                # grab easting and northing for this station
+                easting_match = re_pattern_easting.search(coordinate_contents_line)
+                northing_match = re_pattern_northing.search(coordinate_contents_line)
+                coordinate_dict['Eastings'] = easting_match.group()
+                coordinate_dict['Northings'] = northing_match.group()
+
+                break
+
+        return coordinate_dict
+
 
 def main():
-
     # Create GUI - see GSI Query
 
     # temporary file paths - replace by user chosen within GUI??
     coordinate_file_path = 'Files\\AA9 ARTC_130120.CRD'
+    # coordinate_file_path = 'Files\\AA9 ARTC_130120.STD'
+
     fixed_file_path = 'Files\\AA9 ARTC 030220.FIX'
 
     try:
@@ -113,7 +130,7 @@ def main():
         # open up fixed file & update the fixed file's easting/northings from the coordinate file
         fixed_file = FixedFile(fixed_file_path)
         coordinate_file = CoordinateFile(coordinate_file_path)
-        fixed_file.update(coordinate_file.file_contents)
+        fixed_file.update(coordinate_file)
 
     except Exception as ex:
         print(ex, type(ex))
@@ -124,4 +141,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
