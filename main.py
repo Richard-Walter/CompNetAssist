7993b36eda3e4a83e7d7bcd8d59dfe830762acda
Richard-Walter <willthisnamedo@gmail.com>
2020-02-08 20:56:06 +1100

    reading from a CRD file functinoality

--- main.py
+++ main.py
@@ -1,91 +1,127 @@
-""" This program is assists in survey CompNet adjustments.
+"""
+This program is assists in survey CompNet adjustments.
 
 Written by Richard Walter 2020
 """
 
 import re
 
-"""
-.       - Any Character Except New Line
-\d      - Digit (0-9)
-\D      - Not a Digit (0-9)
-\w      - Word Character (a-z, A-Z, 0-9, _)
-\W      - Not a Word Character
-\s      - Whitespace (space, tab, newline)
-\S      - Not Whitespace (space, tab, newline)
 
-\b      - Word Boundary
-\B      - Not a Word Boundary
-^       - Beginning of a String
-$       - End of a String
+class FixedFile:
 
-[]      - Matches Characters in brackets (Character sets)
-[^ ]    - Matches Characters NOT in brackets
-|       - Either Or
-( )     - Group
+    def __init__(self, fixed_file_path):
 
-Quantifiers:
-*       - 0 or More
-+       - 1 or More
-?       - 0 or One
-{3}     - Exact Number
-{3,4}   - Range of Numbers (Minimum, Maximum)
+        self.fixed_file_path = fixed_file_path
+        self.file_contents = None
+        self.updated_file_contents = ""
+        self.station_list = []
 
-"""
+        with open(fixed_file_path, 'r') as f_orig:
+            self.file_contents = f_orig.readlines()
 
-# use code below to search for easting and northings in a text file
+    # returns a list of stations from the fixed file that need need to be updated
+    def get_station_list(self):
 
-pattern = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d')
-mypattern_easting = re.compile(r'\b28\d{4}\.\d{4}')
-mypattern_northing = re.compile(r'\b62\d{5}\.\d{4}')
-mypattern_easting_and_northing = re.compile(r'\b(28|62)\d{4,5}\.\d{4}') # Need to use iterable on this pattern
+        # Line number is at the start of a string and contains digits followed by whiespace
+        re_pattern = re.compile(r'"\w+"')
+
+        for line in self.file_contents:
+            match = re_pattern.search(line)
+
+            # strip of quotation marks and add to station list
+            self.station_list.append(match.group()[1:-1])
+
+        return self.station_list
+
+    def get_line_number(self, station):
+
+        # Line number is at the start of a line
+        re_pattern = re.compile(r'^\d+\s')
+        line_number = "???"
+
+        for line in self.file_contents:
+
+            if station in line:
+                match = re_pattern.search(line)
+                line_number = match.group()
+                break
+
+        return line_number
+
+    def update(self, coordinate_contents):
+
+        re_pattern_easting = re.compile(r'\b28\d{4}\.\d{4}')
+        re_pattern_northing = re.compile(r'\b62\d{5}\.\d{4}')
+        updated_file_dictionary = {}
+
+        self.station_list = self.get_station_list()
+
+        # check each line of the coordinate contents to see if it contains a matching station.
+        for coordinate_contents_line in coordinate_contents:
+            for station in self.station_list:
+                if station in coordinate_contents_line:
+                    # grab easting and northing for this station
+                    easting_match = re_pattern_easting.search(coordinate_contents_line)
+                    northing_match = re_pattern_northing.search(coordinate_contents_line)
+
+                    # create a dictionary so that we can later order by line number and write out to file
+                    updated_file_dictionary[int(self.get_line_number(station))] = easting_match.group() + '  ' + \
+                                                                                  northing_match.group() + ' ' + '"'\
+                                                                                  + station +'"\n'
+                    break
+
+        # order updated file contents by line number
+        for line_number in sorted(updated_file_dictionary):
+            update_file_line = str(line_number) + ' ' + updated_file_dictionary[line_number]
+            self.updated_file_contents += update_file_line
+
+        print(self.updated_file_contents)
+
+        # update fixed file with updated contents
+        with open(self.fixed_file_path, 'w') as f_update:
+            f_update.write(self.updated_file_contents)
 
 
-# 1 285968.9510 6215310.3810 0.010 0.010 "STN03"
+class CoordinateFile:
 
-try:
-    with open('data.txt', 'r') as f:
+    def __init__(self, coordinate_file_path):
 
-        for line in f:
-            match = mypattern_northing.search(line)
-            print(match.group())
+        self.file_contents = None
 
-except Exception as ex:
-    print(ex)
+        try:
+            with open(coordinate_file_path, 'r') as f_orig:
 
-# use this code to read in a file and replace text.
+                self.file_contents = f_orig.readlines()
 
-original_file_path = 'Files\\AA9 ARTC 030220.FIX'
+        except Exception as ex:
+            print(ex, type(ex))
 
-try:
-    with open(original_file_path, 'r') as f_orig:
+        else:
+            pass
 
-        original_contents = f_orig.readlines()
-        updated_contents = ""
-        old_easting = '500000.000'
-        new_easting = '111111.111'
 
-        old_northing = '10000000.000'
-        new_northing = '22222222.222'
+def main():
 
-        print (original_contents)
+    # Create GUI - see GSI Query
 
-        for line in original_contents:
+    # temporary file paths - replace by user chosen within GUI??
+    coordinate_file_path = 'Files\\AA9 ARTC_130120.CRD'
+    fixed_file_path = 'Files\\AA9 ARTC 030220.FIX'
 
-            # print(line)
+    try:
 
-            updated_line_easting = line.replace(old_easting, new_easting, 1)
-            updated_line_easting_and_northing = updated_line_easting.replace(old_northing, new_northing)
+        # open up fixed file & update the fixed file's easting/northings from the coordinate file
+        fixed_file = FixedFile(fixed_file_path)
+        coordinate_file = CoordinateFile(coordinate_file_path)
+        fixed_file.update(coordinate_file.file_contents)
 
-            updated_contents += updated_line_easting_and_northing
+    except Exception as ex:
+        print(ex, type(ex))
 
-            print(updated_line_easting_and_northing)
+    else:
+        print("SUCCESS")
 
-        print(updated_contents)
 
-        with open(original_file_path, 'w') as f_update:
+if __name__ == '__main__':
+    main()
 
-            f_update.write(updated_contents)
-
-except Exception as ex:
-    print(ex)
